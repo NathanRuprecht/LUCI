3c5e0dc375a68aa719d886cd5cafda5e46d312aa
NathanRuprecht <nathan.a.ruprecht@gmail.com>
2016-10-22 20:16:05 -0500

    Add files via upload

---
+++ LatMap_2/LatMap_2.ino
@@ -0,0 +1,125 @@
+/**********************************
+ * LatMap_2
+ * 
+ * ECE 210 Final Project
+ * C3C Nathan Ruprecht
+ * C3C Brian Yarbrough
+ * 
+ * Hardware:
+ * 1 Ultrasonic sensor
+ * 2 DC Motors
+ * 1 Ardunio Uno
+ * 1 Push Button
+ * 1 9V battery
+ * 
+ * General Description
+ * Measures distances in a 360 degree radius
+ * Stores values to onboard EEPROM
+ * Outputs Values to Serial monitor
+ * 
+ * Values are then independently procesed and mapped with MatLab
+ * 
+ * 10 December 2013
+ * /////////////////////////////////////
+ * Documentation:
+ * Ultrasonic code referencd from http://www.arduino.cc/en/Tutorial/Ping
+ * 
+ *///////////////////////////////////
+
+//NOTE! the Arduino EEPROM has a lifespan of only 100,000 read/writes
+//do not read and write unnecesarily
+#include <EEPROM.h>
+
+//motor pins
+const int mLeft = 11;
+const int mRight = 10;
+//button pin
+const int btn = 8;
+//constant for the pin
+const int pingPin = 7;
+
+//current memory index
+int curMem = 0;
+
+/***********************************
+ * The entire program is run from setup
+ * After initial setup, the program waits for a button push before taking measurments
+ * On the second button push, it will output the values to the serial monitor
+ * These values can then be processed for a 2D map
+ */
+void setup(){
+  //set pinmodes
+  pinMode(mRight, OUTPUT);
+  pinMode(mLeft, OUTPUT);
+  pinMode(btn, INPUT);
+
+  //begin the serial output
+  Serial.begin(9600);
+
+  delay(5);
+
+  //wait for button push to begin
+  while(digitalRead(btn)==LOW){
+    //empty loop
+  }
+
+  //USER PUSHES BUTTON//
+
+  delay(1500);
+  //begin taking measurments
+
+  //power on the motors
+  analogWrite(mRight, 180);
+  analogWrite(mLeft, 180);
+
+  //current runtime of the program
+  int start = millis();
+
+  //run for 3.2 seconds (~1 rotation)
+  while(millis()<start+3200){
+    //take measurements
+    ultraSonicMeasure();
+  }
+
+  //turn off motors
+  digitalWrite(mRight, LOW);
+  digitalWrite(mLeft, LOW);
+
+  delay(50);
+
+  //USER PUSHES BUTTON//
+
+  //wait for button push
+  while(digitalRead(btn)==LOW){
+    //empty loop
+  }
+
+  //print the data to the computer
+  outputData();
+} 
+
+
+/******************************************************
+ * Outputs the stored EEPROM data to the serial monitor on the screen
+ */
+void outputData(){
+  int i;
+  for(i = 0; i<44*8; i+=8){
+    //read the value
+    byte value = EEPROM.read(i);
+    //print the value
+    Serial.println(value);
+  }
+}
+
+//no loop - code only runs once
+void loop(){
+}
+
+
+
+
+
+
+
+
---
+++ LatMap_2/ultraSonicMeasure.ino
@@ -0,0 +1,69 @@
+/*********************************************
+sends out a pulse from the ultrasonic sensor
+listens for the pulse
+returns distance
+*/
+void ultraSonicMeasure(){
+  long duration;
+  byte distance;
+  
+  //pulse timeout (2 times the max range times velocity)
+  long timeOut = 2*255*74;
+
+  //set the pin mode
+  pinMode(pingPin, OUTPUT);
+  //short silence
+  digitalWrite(pingPin, LOW );
+  delayMicroseconds(2);
+  //send out pulse
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW );
+
+  //listen for signal
+  //switch the pin mode
+  pinMode(pingPin, INPUT);
+  //set duration equal to the time
+  duration = pulseIn(pingPin, HIGH, timeOut);
+  //calculate the distance
+  distance =  microsecondsToInches(duration);
+    
+  //store the value in EEPROM
+  store(distance);
+}
+
+/**********************************************
+converts the time for the pulse to return to a measurment in incehs
+passed the pulse time
+*/
+byte microsecondsToInches(long microseconds)
+{
+  // According to Parallax's datasheet for the PING))), there are
+  // 73.746 microseconds per inch 
+
+  //this is the distance
+  long dis = microseconds / 74 / 2;
+  //cast into a byte format so it can be saved
+  if(dis<254){
+    return (byte)dis;
+  }
+  else { //the pulse timed out
+    return 255;
+  }
+}
+
+/*******************************************************
+sotres the measurement
+*/
+void store(byte distance){
+  //check to make sure there is still memory space
+  if(curMem<=1016){
+    //save the value
+    EEPROM.write(curMem, distance);
+    //increment the  memory one byte
+    curMem+=8;
+  }
+}
+
+
+
